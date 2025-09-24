a2c7a1073233bdcc4ad8fa07de64ca642a47d463
A-4-Atom <vcikas2626@gmail.com>
2025-09-24 22:59:59 +0530

    removed boilerplate code and installed zustand

---
+++ app/index.tsx
@@ -0,0 +1,13 @@
+import { Text, View } from 'react-native'
+import React from 'react'
+
+const index = () => {
+  return (
+    <View className='bg-white h-full flex items-center justify-center'>
+      <Text className='text-black'>index</Text>
+    </View>
+  )
+}
+
+export default index
+
